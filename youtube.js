1919b600c3b732658cfc198588f9107a78c3f6af
elijahsuh7 <elijahsuh7@gmail.com>
2020-06-19 16:22:49 +0900

    changed subscription button due to malfuntioning then trimed down JS comment and codes

--- youtube.js
+++ youtube.js
@@ -1,30 +1,34 @@
-const line = document.querySelector(".line");
-const title = document.querySelector(".title.clamp");
-const subBtn = document.querySelector(".subBtn");
-const subDiv = document.querySelector(".subscribe");
-const like = document.querySelector(".like");
-const dislike = document.querySelector(".dislike");
+"use strict";
 
+//title clamping function
+const title = document.querySelector(".title.clamp");
+const line = document.querySelector(".line");
 line.addEventListener("click", () => {
 	line.classList.toggle("clicked");
 	title.classList.toggle("clamp");
 });
 
 /* Subscribe Btn and the alert button*/
+const subBtn = document.querySelector("#subBtn");
+const subDiv = document.querySelector(".subscribe");
 const bellBtn = document.createElement("button");
 const bell = document.createElement("i");
 bell.setAttribute("class", "fas fa-bell");
 bell.style.color = "gray";
 subBtn.addEventListener("click", () => {
 	if (subBtn.className === "subBtn") {
+		subBtn.classList.toggle("active");
 		bellBtn.appendChild(bell);
 		subDiv.appendChild(bellBtn);
 	} else {
+		subBtn.classList.toggle("active");
 		subDiv.removeChild(bellBtn);
 	}
 });
 
 //like and unlike button highlighting when clicked
+const like = document.querySelector(".like");
+const dislike = document.querySelector(".dislike");
 like.addEventListener("click", () => {
 	like.classList.toggle("active");
 	if (dislike.className === "dislike active") {
