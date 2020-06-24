cc0487d2e3cb12e2b3ecfa42e7e1d367452d859e
elijahsuh7 <elijahsuh7@gmail.com>
2020-06-24 22:19:35 +0900

    organized javascript and changed bell showing logic

--- youtube.js
+++ youtube.js
@@ -1,66 +1,76 @@
 "use strict";
 
-//title clamping function
+//clamp button for the title
 const title = document.querySelector(".title.clamp");
 const line = document.querySelector(".line");
 const titleLength = title.textContent.length;
+
+/* Subscribe Btn and the alert button*/
+const subBtn = document.querySelector("#subBtn");
+const subDiv = document.querySelector(".subscribe");
+const bellBtn = document.querySelector("#bell");
+//modal element selection
+const modalContent = document.querySelector(".modal-content");
+const modal = document.querySelector(".modal");
+
+//like and dislike button selection
+const like = document.querySelector(".like");
+const dislike = document.querySelector(".dislike");
+
+line.addEventListener("click", clamp);
+subBtn.addEventListener("click", showBell);
+bellBtn.addEventListener("click", showModal);
+window.addEventListener("click", hideModal);
+like.addEventListener("click", likeVideo);
+dislike.addEventListener("click", dislikeVideo);
+
 if (titleLength < 71) {
 	line.style.display = "none";
 } else {
 	line.style.display = "inline-block";
 }
 
-line.addEventListener("click", () => {
+//like and unlike button highlighting when clicked
+
+function clamp() {
 	line.classList.toggle("clicked");
 	title.classList.toggle("clamp");
-});
+}
 
-/* Subscribe Btn and the alert button*/
-const subBtn = document.querySelector("#subBtn");
-const subDiv = document.querySelector(".subscribe");
-const bellBtn = document.createElement("button");
-bellBtn.classList = "bellBtn";
-const bell = document.createElement("i");
-bell.classList = "fas fa-bell";
-bell.style.color = "gray";
-subBtn.addEventListener("click", () => {
+function showBell() {
 	if (subBtn.className === "subBtn") {
 		subBtn.classList.toggle("active");
-		bellBtn.appendChild(bell);
-		subDiv.appendChild(bellBtn);
+		bellBtn.style.display = "inline-block";
 	} else {
 		subBtn.classList.toggle("active");
-		subDiv.removeChild(bellBtn);
+		bellBtn.style.display = "none";
 	}
-});
+}
 
-const modalContent = document.querySelector(".modal-content");
-bellBtn.addEventListener("click", () => {
-	modal.style.display = "block";
-	modalContent.style.display = "flex";
-});
+function showModal(event) {
+	if (event.target === bellBtn) {
+		modal.style.display = "block";
+		modalContent.style.display = "flex";
+	}
+}
 
-const modal = document.querySelector(".modal");
-window.addEventListener("click", (event) => {
+function hideModal(event) {
 	if (event.target === modal) {
 		modal.style.display = "none";
 		modalContent.style.display = "none";
 	}
-});
+}
 
-//like and unlike button highlighting when clicked
-const like = document.querySelector(".like");
-const dislike = document.querySelector(".dislike");
-like.addEventListener("click", () => {
+function likeVideo() {
 	like.classList.toggle("active");
 	if (dislike.className === "dislike active") {
 		dislike.classList.toggle("active");
 	}
-});
+}
 
-dislike.addEventListener("click", () => {
+function dislikeVideo() {
 	dislike.classList.toggle("active");
 	if (like.className === "like active") {
 		like.classList.toggle("active");
 	}
-});
+}
